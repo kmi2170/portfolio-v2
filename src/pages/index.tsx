3ee91d7b737359bb7f077c5d80047cb2575b795a
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2023-09-14 22:37:25 -0700

    Fix: About props
    	modified:   src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,21 @@
-import type {
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-  NextPage,
-} from 'next';
-import { client } from '../lib/sanity';
-import styled from 'styled-components';
+import type { GetServerSideProps, InferGetServerSidePropsType, NextPage } from "next";
+import { client } from "../lib/sanity";
+import styled from "styled-components";
 
-import { Navbar, Header, Projects, About, Skills, Footer } from '../components';
-import { DataAbout, DataProject, DataSkills, Lang } from '../lib/types';
-import { data } from '../assets/projects';
-import { useState } from 'react';
+import { Navbar, Header, Projects, About, Skills, Footer } from "../components";
+import { DataAbout, DataProject, DataSkills, Lang } from "../lib/types";
+import { data } from "../assets/projects";
+import { useState } from "react";
 
-const Home: NextPage = ({
-  dataAbout,
-  dataProjects,
-  dataSkills,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const [lang, setLang] = useState<Lang>('jp');
+const Home: NextPage = ({ dataAbout, dataProjects, dataSkills }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [lang, setLang] = useState<Lang>("jp");
 
   return (
     <Wrapper>
       <Navbar setLang={setLang} />
       <Header />
       <Projects projects={dataProjects} lang={lang} />
-      <About about={dataAbout} lang={lang} />
+      <About lang={lang} />
       <Skills skills={dataSkills} />
     </Wrapper>
   );
@@ -36,9 +28,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   const query_about = '*[_type == "about"]';
   const query_skills = '*[_type == "skills"]';
 
-  const [dataAbout, dataSkills] = await Promise.all<
-    [DataAbout[], DataSkills[]]
-  >([client.fetch(query_about), client.fetch(query_skills)]);
+  const [dataAbout, dataSkills] = await Promise.all<[DataAbout[], DataSkills[]]>([client.fetch(query_about), client.fetch(query_skills)]);
 
   // const dataAbout = null;
   // const dataSkills = [];
