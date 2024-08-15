1d56402703c6bb2613f3ec20bb0971065aa7032b
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-08-14 23:45:44 -0700

    fix unknown keyword

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
 module.exports = {
-  nextConfig,
+  reactStrictMode: true,
   compiler: {
-    // ssr and displayName are configured by default
     styledComponents: true,
   },
-}
+};
