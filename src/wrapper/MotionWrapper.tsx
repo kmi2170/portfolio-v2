f1911bc882a0fa216cc5a9202f0a85d06b6dce5d
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-10-03 20:34:08 -0700

    minor modification

--- src/wrapper/MotionWrapper.tsx
+++ src/wrapper/MotionWrapper.tsx
@@ -1,13 +1,16 @@
 import { motion } from "framer-motion";
 
-const MotionWrapper = <P extends object>(Component: React.ComponentType<P>) => {
-  const WrappedComponent = (props: P) => (
-    <motion.div whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }} transition={{ duration: 0.75, ease: "easeInOut" }}>
-      <Component {...props} />
-    </motion.div>
-  );
-
-  return WrappedComponent;
+const MotionWrapper = <P extends {}>(Component: React.ComponentType<P>) => {
+  return function Wrapper(props: P) {
+    return (
+      <motion.div
+        whileInView={{ y: [100, 50, 0], opacity: [0, 0.25, 1] }}
+        transition={{ duration: 1.0, ease: "easeInOut" }}
+      >
+        <Component {...props} />
+      </motion.div>
+    );
+  };
 };
 
 export default MotionWrapper;
